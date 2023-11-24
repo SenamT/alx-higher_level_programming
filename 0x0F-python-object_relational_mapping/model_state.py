e4b34006c451f210962f9d188c49c8b9aacda300
SenamT <senny2513@gmail.com>
2023-11-24 15:07:43 -0800

    Object-relational mapping

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+this will containt the states class and its base
+"""
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+
+class State(Base):
+    """
+    this is the class which has the ID and the name attributes for each state
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
